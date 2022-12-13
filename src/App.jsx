07c34c509a9c4cba67ea37cc92785d38d94e63d4
greg <gregorybwest@gmail.com>
2022-12-13 14:20:52 -0800

    allow '/' in place of 'http://localhost3000' in development mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Header } from "./Header";
 import { Home } from "./Home";
 import { Footer } from "./Footer";
+import axios from "axios";
 
 function App() {
+  // eslint-disable-next-line no-undef
+  axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+
   return (
     <div>
       <Header />
