5e1c906fc1b617a4e58abaf2688a713942e5ca4b
greg <gregorybwest@gmail.com>
2022-12-13 12:22:53 -0800

    displays all airports by name, code, and address, and their corresponding FBOs with average rating

--- src/AirportsIndex.jsx
+++ src/AirportsIndex.jsx
@@ -4,8 +4,12 @@ export function AirportsIndex(props) {
       <h1>All Airports</h1>
       {props.airports.map((airport) => (
         <div key={airport.id}>
-          <h2>{airport.name}</h2>
-          <p>Adress: {airport.address}</p>
+          <h1>{airport.name} ({airport.code})</h1>
+          <h4>Address: {airport.address}</h4>
+          <h3>FBOs: {airport.users.map((user) => (
+            <p key={user.id}>
+              {user.user_name}, Rating: {user.average_rating}</p>
+          ))}</h3>
         </div>
       ))}
     </div>
