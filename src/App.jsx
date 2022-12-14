6b4fd6ba33ce6ca71ef3171415f5ca474ff8dede
greg <gregorybwest@gmail.com>
2022-12-13 20:08:06 -0800

    browser router with route to users-show page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,25 @@ import { Header } from "./Header";
 import { Home } from "./Home";
 import { Footer } from "./Footer";
 import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { UsersShow } from "./UsersShow"
+
 
 function App() {
   // eslint-disable-next-line no-undef
   axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
   return (
-    <div>
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+      <BrowserRouter>
+        <div>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/users/:id" element={<UsersShow />}></Route>
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
   );
 }
 
